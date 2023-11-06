bf487327fac66358ef255f212879a8c34c846205
Yasir Aris M <git@yasirdev.my.id>
2023-11-06 09:57:19 +0700

    Update dev.py
    Signed-off-by: Yasir Aris M <git@yasirdev.my.id>

--- misskaty/plugins/dev.py
+++ misskaty/plugins/dev.py
@@ -537,7 +537,6 @@ async def update_restart(_, ctx: Message, strings):
 
 @app.on_raw_update(group=-99)
 async def updtebot(client, update, users, _):
-    client.log.info(update)
     if isinstance(update, UpdateBotStopped):
         niuser = users[update.user_id]
         if update.stopped and await db.is_user_exist(niuser.id):
